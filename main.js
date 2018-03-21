ffa1a570690208c4990bb8e894d5b64074529b82
Mary Alice Orr <m.a.orr17@gmail.com>
2018-03-20 21:25:52 -0500

    added button

--- main.js
+++ main.js
@@ -68,10 +68,18 @@ const buildDomString = (studentArray) => {
        domString +=     `<h1>${student.firstName} ${student.lastName}</h1>`;
        domString +=     `<h3>${student.catchPhrase}</h3>`;
        domString +=     `<img src="${student.avatar}" alt="">`;
-       domString +=     `<button class="button">Brought Pie</button>`;
+       domString +=     `<button class="card-button">Brought Pie</button>`;
        domString += `</div>`;
     });
     printToDom(domString, 'card-holder');
 }
 
 buildDomString(students);
+
+const allTheButtons = document.getElementsByClassName("card-button");
+
+for (let i=0; i<allTheButtons.length; i++) {
+    allTheButtons[i].addEventListener("click", (e) => {
+        console.log('event!!!!', e);
+    });
+}
