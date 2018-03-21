fbbeec1e6afb81a860e7114bc59f41a842634421
Mary Alice Orr <m.a.orr17@gmail.com>
2018-03-20 21:11:24 -0500

    added cards

--- main.js
+++ main.js
@@ -55,3 +55,23 @@ const students = [
         catchPhrase: "Multi-channelled solution-oriented artificial intelligence"
     }
 ];
+
+const printToDom = (domString, divId) => {
+    const printToDom = document.getElementById(divId);
+    printToDom.innerHTML = domString;
+};
+
+const buildDomString = (studentArray) => {
+    let domString = " ";
+    studentArray.forEach ((student) => {
+       domString += `<div class="card">`;
+       domString +=     `<h1>${student.firstName} ${student.lastName}</h1>`;
+       domString +=     `<h3>${student.catchPhrase}</h3>`;
+       domString +=     `<img src="${student.avatar}" alt="">`;
+       domString +=     `<button class="button">Brought Pie</button>`;
+       domString += `</div>`;
+    });
+    printToDom(domString, 'card-holder');
+}
+
+buildDomString(students);
